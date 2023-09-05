91c5363e33a73ebebe5b71a7181ed7767b9ccf6d
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2023-09-04 21:56:50 -0400

    almost

--- courses/artificialintelligence/assignments/flocking/flocking.cpp
+++ courses/artificialintelligence/assignments/flocking/flocking.cpp
@@ -22,9 +22,19 @@ struct Vector2D
     return Vector2D(x * rhs, y * rhs);
   }
 
+  Vector2D operator/ (const float& rhs) const
+  {
+    return Vector2D(x / rhs, y / rhs);
+  }
+
   Vector2D operator- (const Vector2D& rhs) const
   {
     return Vector2D(x - rhs.x, y - rhs.y);
+  }
+
+  Vector2D operator+ (const Vector2D& rhs) const
+  {
+    return Vector2D(x + rhs.x, y + rhs.y);
   }
 
   double x;
@@ -42,13 +52,15 @@ public:
 
   void update(double deltaTime)
   {
+    Vector2D force = m_cohesionForce + m_separationForce + m_alignmentForce;
+    m_velocity += force * deltaTime;
     m_position += m_velocity * deltaTime;
     std::cout << std::fixed << std::setprecision(3)<< m_position.x << " " << m_position.y << " " << m_velocity.x << " " << m_velocity.y << std::endl;
   }
 
-  void calculateCohesion(double radius, Agent agents[], int numOfAgents, double deltaTime)
+  void calculateCohesion(double constant, double radius, Agent agents[], int numOfAgents)
   {
-    calculateNeighbors(radius, agents, numOfAgents);
+    calculateNeighbors(radius, agents, numOfAgents, false);
 
     Vector2D center;
     for (auto i = neighborhood.begin(); i < neighborhood.end(); ++i)
@@ -59,12 +71,37 @@ public:
 
     Vector2D force;
     force = center - m_position;
-    m_velocity += force * deltaTime;
+    m_cohesionForce = force * constant;
   }
 
-  void calculateCohesion()
+  void calculateSeparation(double constant, double maxForce, double radius, Agent agents[], int numOfAgents)
   {
+    calculateNeighbors(radius, agents, numOfAgents, false);
 
+    Vector2D force;
+    for (auto i = neighborhood.begin(); i < neighborhood.end(); ++i)
+    {
+      force.x += m_position.x - i->getPos().x;
+      force.y += m_position.y - i->getPos().y;
+    }
+
+    if(force.x > maxForce) force.x = maxForce;
+    if(force.y > maxForce) force.y = maxForce;
+    m_separationForce = force * constant;
+  }
+
+  void calculateAlignment(double constant, double radius, Agent agents[], int numOfAgents)
+  {
+    calculateNeighbors(radius, agents, numOfAgents, true);
+
+    Vector2D averageVelocity;
+    for (auto i = neighborhood.begin(); i < neighborhood.end(); ++i)
+    {
+      averageVelocity += i->getVel();
+    }
+
+    averageVelocity = averageVelocity / neighborhood.size();
+    m_alignmentForce = averageVelocity * constant;
   }
 
   Vector2D getPos()
@@ -78,11 +115,14 @@ public:
   }
 
 private:
-  void calculateNeighbors(double radius, Agent agents[], int numOfAgents)
+  void calculateNeighbors(double radius, Agent agents[], int numOfAgents, bool includeSelf)
   {
+    neighborhood.clear();
+
     for (int i = 0; i < numOfAgents; i++)
     {
-      if (&agents[i] == this) continue;
+      if(!includeSelf)
+        if (&agents[i] == this) continue;
 
       Vector2D agentPos = agents[i].getPos();
       if(agentPos.x > m_position.x + radius || agentPos.x < m_position.x - radius) continue;
@@ -93,6 +133,7 @@ private:
   }
 
   Vector2D m_position, m_velocity;
+  Vector2D m_cohesionForce, m_separationForce, m_alignmentForce;
   std::vector<Agent> neighborhood;
 };
 
@@ -118,15 +159,20 @@ int main()
     //std::cout << std::fixed << std::setprecision(3)<< agents[i].getPos().x << " " << agents[i].getPos().y << " " << agents[i].getVel().x << " " << agents[i].getVel().y << std::endl;
   }
 
-  std::cin >> deltaTime;
-
-  for (int i = 0; i < numOfAgents; i++)
+  while (std::cin >> deltaTime)
   {
-    agents[i].calculateCohesion(c_radius, agents, numOfAgents, deltaTime);
+    for (int i = 0; i < numOfAgents; i++)
+    {
+      agents[i].calculateCohesion(c_constant, c_radius, agents, numOfAgents);
+      agents[i].calculateSeparation(s_constant, maxForce, s_radius, agents, numOfAgents);
+      agents[i].calculateAlignment(a_constant, a_radius, agents, numOfAgents);
+    }
+
+    for (int i = 0; i < numOfAgents; i++)
+    {
+      agents[i].update(deltaTime);
+    }
   }
 
-  for (int i = 0; i < numOfAgents; i++)
-  {
-    agents[i].update(deltaTime);
-  }
+  delete[] agents;
 }
