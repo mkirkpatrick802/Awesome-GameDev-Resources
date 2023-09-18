996ef26aa7d9c3a00263e1d6d97c399417f0fdd8
Michael Kirkpatrick <michael.kirkpatrick@mymail.champlain.edu>
2023-09-18 13:46:32 -0400

    random done

--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,21 @@
-// add your imports here
-int main(){
-  // code here
+#include <iostream>
+
+int main()
+{
+  unsigned int seed, count;
+  signed int min, max;
+  std::cin >> seed >> count >> min >> max;
+
+  unsigned int random = seed;
+  for (int i = 0; i < count; i++)
+  {
+    random = random xor (random << 13);
+
+    random = random xor (random >> 17);
+
+    random = random xor (random << 5);
+
+    unsigned int value = min + (random % (max - min + 1));
+    std::cout << value << std::endl;
+  }
 }
